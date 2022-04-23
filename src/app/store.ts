13d8e48dd3aaed113ac5bb4e53f7f551faeb4e33
Rony Derra <derrarony@gmail.com>
2022-04-23 22:27:36 +0300

    store confi

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import dndTableReducer from '../features/dndTable/dndTableSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    dndTableData: dndTableReducer,
   },
 });
 
